801de0d9e0114e5e8e7e59f999fc982c582c50fb
Bhavya P R <130534008+Bhavya-PR@users.noreply.github.com>
2026-06-16 17:19:16 +0530

    Implement Weird Algorithm to generate sequence

---
+++ Introductory Problems/Weird Algorithm.py
@@ -0,0 +1,12 @@
+n = int(input("enter the value of n: "))
+res =[]
+res.append(n)
+
+while n !=1:
+    if n %2==0:
+        n=n//2
+        res.append(n)
+    else:
+        n=(n*3)+1
+        res.append(n)
+print(res)
